76de9561f2846cf337afdc89008f658c8a67a461
tomoyanonymous <anon.spyc@gmail.com>
2017-08-14 01:24:20 +0900

    fixed condition in option httpd and sound2faust, fixed some script according to brew-audit.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -5,11 +5,9 @@ class Faust < Formula
   option "with-httpd", "compile httpdlib (requires GNU libmicrohttpd)"
   option "with-dynamic", "compile httpd & osc supports as dynamic libraries"
   option "with-sound2faust", "compile sound to DSP file converter"
-  option "with-test","install test suite"
+  option "with-test", "install test suite"
 
   depends_on "pkg-config" => :run
-  depends_on "libmicrohttpd" =>[:run,"with-httpd"]
-  depends_on "libsndfile" => [:run,"with-sound2faust"]
 
   if build.without? "faust0"
     depends_on "llvm@3.8" => :run
@@ -26,26 +24,34 @@ class Faust < Formula
     sha256 "abb1dee52faf427d232f56f2037ba5e81cb60a9e6684ac10f41da6c5c6df3415"
     head "https://github.com/grame-cncm/faust.git", :branch => "master"
   end
+  if build.with? "httpd"
+    depends_on "libmicrohttpd" =>:run
+  end
+  if build.with? "sound2faust"
+    depends_on "libsndfile" => :run
+  end
 
   def install
     system "make"
     if build.with? "httpd"
-      system "make","httpd"
+      system "make", "httpd"
     end
     if build.with? "dynamic"
-      system "make","dynamic"
+      system "make", "dynamic"
     end
     if build.with? "sound2faust"
-      system "make","sound2faust"
+      system "make", "sound2faust"
     end
     system "make", "install", "PREFIX=#{prefix}"
     if build.with? "test"
-      system "cp -r tests #{prefix}"
+      cp_r "tests", prefix
     end
   end
   test do
-    system "cp -r #{prefix}/tests/architecture-tests #{testpath}"
-    system "cd #{testpath}/architecture-tests &&./testfailure"
-    system "cd #{testpath}/architecture-tests &&./testsuccess osx"
+    cp_r "#{prefix}/tests/architecture-tests", testpath
+    cd "#{testpath}/architecture-tests"
+    system "./testfailure"
+    cd "#{testpath}/architecture-tests"
+    system "./testsuccess", "osx"
   end
 end
