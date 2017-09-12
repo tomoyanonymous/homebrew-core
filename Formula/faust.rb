9fc7e264f0fc043898769cc7c5b3d47ec297d12d
tomoyanonymous <anon.spyc@gmail.com>
2017-09-12 13:16:59 +0900

    removed without-xxx options.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -6,34 +6,28 @@ class Faust < Formula
   sha256 "9bd0b02020a6d9130ac3e2b11ad7cbfc03883769eb87dcb76251c80c40488494"
   head "https://github.com/grame-cncm/faust.git", :branch => "faust2"
 
-  option "without-httpd", "don't compile httpdlib (requires GNU libmicrohttpd)"
-  option "without-dynamic", "don't compile httpd & osc supports as dynamic libraries"
-  option "without-sound2faust", "don't compile sound to DSP file converter"
-  option "without-test", "don't install test suite"
-  depends_on "pkg-config" => :run
-  depends_on "llvm" => :run
-  depends_on "openssl" => :run
-  if build.with? "httpd"
-    depends_on "libmicrohttpd" =>:run
-  end
-  if build.with? "sound2faust"
-    depends_on "libsndfile" => :run
-  end
+  depends_on "pkg-config" => "build"
+  depends_on "llvm"
+  depends_on "openssl"
+  depends_on "libmicrohttpd"
+  depends_on "libsndfile"
 
   def install
-    if !build.head?
-      inreplace "compiler/Makefile.unix", "$(filter $(LLVM_VERSION), 4.0.0)", "$(filter $(LLVM_VERSION), 4.0.0 4.0.1)"
+    unless build.head?
+      inreplace "compiler/Makefile.unix", "else ifeq ($(LLVM_VERSION),$(filter $(LLVM_VERSION), 4.0.0))
+    LLVM_VERSION = LLVM_40
+    CLANGLIBS=$(CLANGLIBSLIST)
+    CXXFLAGS += -std=gnu++11", "else ifeq ($(LLVM_VERSION),$(filter $(LLVM_VERSION), 4.0.0 4.0.1))
+    LLVM_VERSION = LLVM_40
+    CLANGLIBS=$(CLANGLIBSLIST)
+    CXXFLAGS += -std=gnu++11
+
+else ifeq ($(LLVM_VERSION),$(filter $(LLVM_VERSION), 5.0.0))
+    LLVM_VERSION = LLVM_50
+    CLANGLIBS=$(CLANGLIBSLIST)
+    CXXFLAGS += -std=gnu++11"
     end
-    system "make"
-    if build.with? "httpd"
-      system "make", "httpd"
-    end
-    if build.with? "dynamic"
-      system "make", "dynamic"
-    end
-    if build.with? "sound2faust"
-      system "make", "sound2faust"
-    end
+    system "make world"
     system "make", "install", "PREFIX=#{prefix}"
     if build.with? "test"
       cp_r "tests", prefix
